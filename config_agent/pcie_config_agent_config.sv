75b32da9fe0240c86c95b2ad7fdb3e4b90f1b1f3
saravana kumar T J A <151965277+saravana-kumar-T-J-A@users.noreply.github.com>
2026-08-31 18:13:26 +0530

    Create pcie_config_agent_config.sv

---
+++ config_agent/pcie_config_agent_config.sv
@@ -0,0 +1,13 @@
+`include "top.svh"
+`include "uvm_macros.svh"
+
+import uvm_pkg::*;
+
+class pcie_config_agent_config extends uvm_object;
+  `uvm_object_utils(pcie_config_agent_config)
+  virtual pcie_if vif;
+  uvm_active_passive_enum is_active = UVM_ACTIVE;
+  function new(string name = "pcie_config_agent_config");
+    super.new(name);
+  endfunction
+endclass
